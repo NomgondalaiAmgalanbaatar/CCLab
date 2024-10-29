d1f86024b683c56d6bc102b942040afb7d1a2301
Nomgo <amgaanomgoo@gmail.com>
2024-10-29 17:05:51 +0800

    new

--- sound-project/js/script.js
+++ sound-project/js/script.js
@@ -11,6 +11,12 @@ function setup() {
 
 function draw() {
     background(0);
+
+    let vol = map(mouseY, 0, height, 0.0, 1.0, true)
+    sound.setVolume(vol);
+
+    let panVal = map(mouseX, 0, width, 1.0, -1.0, true)
+    sound.pan(panVal);
 }
 
 function mousePressed() {
