b1d16d9776995ee8cc699839b27164718d8eb424
Nomgo <amgaanomgoo@gmail.com>
2024-11-11 19:57:52 +0800

    new

--- Blank Template/js/sketch.js
+++ Blank Template/js/sketch.js
@@ -1,67 +1,86 @@
-
 let particles = [];
-let num = 100;
 
 function setup() {
   let canvas = createCanvas(500, 400);
   canvas.parent("p5-canvas-container");
-  background(220);
-
-  for (let i = 0; i < num; i++);
-  let x = random(width);
-  let y = random(width);
-  let rad = random(10, 20);
-  particles.push(new Particle(x, y, rad));
 
 }
 
 function draw() {
-  //
-  background(0, 50);
+  background(0);
+
+  let x = mouseX;
+  let y = mouseY;
+  let dia = random(30, 40);
+  let ball = new Ball(x, y, dia);
+  particles.push(ball);
+
   for (let i = 0; i < particles.length; i++) {
-    let p = particles[i]
+    let p = particles[i];
     p.move();
+    p.checkOutOfCanvas();
+    p.age();
     p.display();
-    p.reappear();
+  }
+  //
+  // while (particles.length > 300) {
+  //   particles.splice(0, 1);
+  // }
+
+  for (let i = particles.length - 1; i >= 0; i--) {
+    let p = particles[i];
+    if (p.isDone == true) {
+      particles.splice(i, 1);
+    }
   }
 
+  fill(255, 0, 0);
+  text("number of objects: " + particles.length, 10, 30);
 }
 
-class Particle {
-  constructer(x, y, rad) {
+class Ball {
+  constructor(x, y, dia) {
     this.x = x;
     this.y = y;
-    this.rad = rad;
-    this.xSpeed = random(1, 3);
-    this.ySpeed = random(1, 3);
-    this.r = random(255);
-    this.g = random(255);
-    this.b = random(255);
-  }
+    this.dia = dia;
+    this.xSpeed = random(-2, 2);
+    this.ySpeed = random(-2, 2);
+    this.isDone = false;
 
+    this.r = 0;
+    this.g = 255;
+    this.b = 255;
+
+    this.lifespan = 1.0;
+    this.lifeReduce = random(0.001, 0.01);
+  }
   move() {
     this.x += this.xSpeed;
     this.y += this.ySpeed;
   }
-
-  reappear() {
-    if (this.x > width) {
-      this.x = 0;
-    } else if (this.x < 0) {
-      this.x = width;
-    } if (this.y > height) {
-      this.y = 0;
-    } else if (this.y < 0) {
-      this.y = height;
+  age() {
+    if (this.lifespan > 0) {
+      this.lifespan -= this.lifeReduce;
+    } else {
+      this.lifespan = 0;
+      this.isDone = true;
     }
   }
-
+  checkOutOfCanvas() {
+    if (this.x > width || this.x < 0 || this.y > height || this.y < 0) {
+      // out!
+      // this.dia = 100;
+      this.isDone = true;
+    }
+  }
   display() {
     push();
+    translate(this.x, this.y);
+    fill(this.r + 255 * this.lifespan, this.g, this.b);
     noStroke();
-    fill(this.r, this.g, this.b);
-    textSize(28);
-    text("STAR", this.x, this.y);
+    circle(0, 0, this.dia * this.lifespan);
+    fill(255, 0, 0);
+    text(this.lifespan, 0, 0);
     pop();
   }
 }
--- Object dancers/js/sketch.js
+++ Object dancers/js/sketch.js
@@ -1,9 +1,53 @@
+let particles = [];
+
 function setup() {
   let canvas = createCanvas(500, 400);
   canvas.parent("p5-canvas-container");
-  background(220);
 }
 
 function draw() {
-  //
+  background(20, 30, 60);
+
+  if (particles.length < 300) {
+    particles.push(new Particle(random(width), random(-50, 0)));
+  }
+
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].display();
+
+    if (particles[i].y > height) {
+      particles.splice(i, 1);
+    }
+  }
+}
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.length = random(10, 20);
+    this.dia = 1;
+    this.speed = random(4, 10);
+    this.angle = PI / 6;
+    this.color = color(135, 206, 250, 150);
+  }
+
+  update() {
+    this.y += this.speed;
+
+    this.x += sin(this.angle) * 0.5;
+  }
+
+  display() {
+    push();
+    translate(this.x, this.y);
+    rotate(this.angle);
+    noStroke();
+    fill(this.color);
+
+    ellipse(0, 0, this.dia, this.length);
+
+    pop();
+  }
 }
